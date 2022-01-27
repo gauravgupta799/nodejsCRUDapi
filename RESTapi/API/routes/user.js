3d866ba669d186beecb47b5a37a77c71cd88c0a2
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-01-27 18:48:18 +0530

    updated

--- RESTapi/API/routes/user.js
+++ RESTapi/API/routes/user.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../model/user')
 const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+
 
 // Signup api
 router.post('/', (req, res,next) => {
@@ -39,46 +39,5 @@ router.post('/', (req, res,next) => {
         }
     })
 })
-
-// Login api
-
-router.post('/login', (req, res) => {
-    User.find({username: req.body.username})
-    .exac()
-    .then(User=>{
-        if(User.length < 1){
-            return res.status(401).json({
-                message: 'User in not exist'
-            })
-        }
-        bcrypt.compare(req.body.password, User[0].password, (err, result)=>{
-            if(!result){
-                return res.status(401).json({
-                    message: 'Password is not matched'
-                })
-            }
-            if(result){
-              const Token = jwt.sign({
-                  username:User[0].username,
-                  userType:User[0].userType,
-                  phone:User[0].phone,
-                  city:User[0].city
-              },'This is a token',{expireIn:"24h"}
-              )
-              res.status(200).json({
-                  username:User[0].username,
-                  userType:User[0].userType,
-                  phone:User[0].phone,
-                  token:Token
-              })
-            }
-        })
-    })
-    .catch(err =>{
-        res.status(500).json({
-            error:err
-        })
-    })
- })
 
 module.exports = router;
