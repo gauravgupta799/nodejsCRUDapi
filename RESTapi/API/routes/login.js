28b6191296e0a2e571752364733c5cfbed2372fc
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-01-27 18:43:24 +0530

    login api

---
+++ RESTapi/API/routes/login.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../model/user')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
+// Login
+    router.post('/',(req, res,next) => {
+    User.find({username: req.body.username})
+    .exac()
+    .then(user=>{
+        if(user.length < 1){
+            return res.status(401).json({
+                message: 'User is not exist'
+            })
+        }
+        bcrypt.compare(req.body.password, User[0].password, (err, result)=>{
+            if(!result){
+                return res.status(401).json({
+                    message: 'Password is not matched'
+                })
+            }
+            if(result){
+              const Token = jwt.sign({
+                  username:User[0].username,
+                  userType:User[0].userType,
+                  phone:User[0].phone,
+                  city:User[0].city,
+              },'this is a valid token',
+              {
+                  expireIn:"24h"
+              });
+              res.status(200).json({
+                  username:User[0].username,
+                  userType:User[0].userType,
+                  phone:User[0].phone,
+                  token:Token
+              })
+            }
+        })
+    })
+    .catch(err =>{
+        res.status(500).json({
+            error:err
+        })
+    })
+ })
+
+
+
+
+module.exports = router;
+
+
