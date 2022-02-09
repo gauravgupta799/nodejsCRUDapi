c85abd40beb341aeebbad4e806771b591cce4f3f
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-02-09 19:24:16 +0530

    MVC

--- RESTapi/app.js
+++ RESTapi/app.js
@@ -1,35 +1,38 @@
 const express = require("express");
 const app = express();
+const path = require('path');
 const port = process.env.PORT || 8800;
 const mongoose = require("mongoose");
-const userroute = require("./API/routes/user");
+const dotenv = require('dotenv');
+dotenv.config();
+const MONGODB = process.env.MONGODB_URI;
+
+const RegisterUser = require("./API/routes/signup");
 const logoutroute = require("./API/routes/logout");
 const loginroute = require("./API/routes/login");
 const homeroute = require("./API/routes/home");
 const bodyParser = require("body-parser");
 const profileroute = require("./API/routes/profile");
 
-const mongourl = "mongodb+srv://gauravcom:gupta123@cluster0.wzm9s.mongodb.net/API?retryWrites=true&w=majority";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use("/api/user", userroute);
+app.use("/api/user", RegisterUser);
 app.use("/api/user", homeroute);
 app.use("/api/user", loginroute);
 app.use("/api/user", logoutroute);
 app.use("/api/user", profileroute);
 
-app.use("/uploads", express.static("uploads"));
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, "./uploads")));
 
-app.use((req, res, next) => {
+app.use((req, res) => {
 	res.status(200).json({
 		message: "app is running",
 	});
 });
 
-mongoose.connect(mongourl, {useNewUrlParser: true,useUnifiedTopology: true})
+mongoose.connect(MONGODB, {useNewUrlParser: true,useUnifiedTopology: true})
 	.then(console.log("Connected with database successfully.."))
 	.catch((err) => console.error(err));
 
