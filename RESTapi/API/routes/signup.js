7c2ba807e1abd492a37b1814e0ba82754edd9c79
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-02-21 23:50:12 +0530

    Delete RESTapi/API directory

--- RESTapi/API/routes/signup.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const RegisterUser = require("../controllers/signup");
-
-
-// Register
-router.post("/signup", RegisterUser);
-
-module.exports = router;
